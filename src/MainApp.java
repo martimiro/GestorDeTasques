b0b53ce922d89605f7937b10c5b90d36e53bab5e
mrt <mmirobar17@alumnes.ub.edu>
2025-10-26 16:18:19 +0100

    creació de tots els fitxers necessàris pel projecte

---
+++ src/MainApp.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class MainApp {
+    public static void main(String[] args) {
+    }
+}
